4054c3937b6dc2bea580e43e91763c86bf5f9b75
Lam Nhan <lamnhan92@gmail.com>
2019-08-20 12:41:19 +0700

    migrate index & utils

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,13 @@
-export function something() {
-	return true;
+function onOpen(e) {
+  SpreadsheetApp.getUi()
+    .createMenu('Sheetbase')
+    .addItem('JSON Editor', 'jsonEditorSidebar')
+    .addItem('HTML Editor', 'htmlEditorSidebar')
+    .addSeparator()
+    .addItem('Taxonomy Editor', 'taxonomyEditorSidebar')
+    .addToUi();
+}
+
+function onInstall(e) {
+  return onOpen(e);
 }
