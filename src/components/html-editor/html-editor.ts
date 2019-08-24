9cfb07de5726f0b2d79ec8619b32df76750ba56a
Lam Nhan <lamnhan92@gmail.com>
2019-08-24 22:06:06 +0700

    add toolbar drawer

--- src/components/html-editor/html-editor.ts
+++ src/components/html-editor/html-editor.ts
@@ -23,6 +23,7 @@ tinymce.init({
   // menu
   menubar: 'file edit view insert format tools table tc help',
   // toolbar
+  toolbar_drawer: 'sliding',
   toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist | forecolor backcolor casechange permanentpen formatpainter removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media pageembed template link anchor codesample | a11ycheck ltr rtl | showcomments addcomment' + ' | code paste spellchecker',
   selection_toolbar: 'bold italic | quicklink h2 h3 blockquote quickimage quicktable',
 } as any);
